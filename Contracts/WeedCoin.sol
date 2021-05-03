ef872a17f42658e21284ce6a5a07a67eb0861ea8
WeedCoin <weedcoinproject@gmail.com>
2021-05-02 21:23:49 -0700

    Update WeedCoin.sol

--- Contracts/WeedCoin.sol
+++ Contracts/WeedCoin.sol
@@ -10,17 +10,18 @@ contract WeedCoin is Context, ERC20Deflationary {
     string private name_ = "WeedCoin";
     string private symbol_ = "WEED";
     uint8 private decimal_ = 9;
-    uint256 private totalSupply_ = 10 ** 15;
-    uint8 private taxBurn_ = 10;
-    uint8 private taxReward_ = 10;
-    uint8 private taxLiquify_ = 10;
-    uint256 private minTokensBeforeSwap_ = (10 ** 7) * (10 ** decimal_); 
-    address private pancakeswapV2Router = 0x10ED43C718714eb63d5aA57B78B54704E256024E;
-
-    constructor () ERC20Deflationary("WeedCoin", "WEED", 18, totalSupply_) {
-        enableAutoBurn(taxBurn_);
-        enableReward(taxReward_);
-        enableAutoSwapAndLiquify(taxLiquify_, pancakeswapV2Router, minTokensBeforeSwap_);
+    uint256 private tokenSupply_ = 420 * (10 ** 12) + 69;
+    uint8 private taxBurn_ = 2;
+    uint8 private taxReward_ = 3;
+    uint8 private taxLiquify_ = 5;
+    uint8 private taxDecimals_ = 0;
+    uint256 private minTokensBeforeSwap_ = (10 ** 7) * (10 ** decimal_);
+    address private routerAddress = 0x10ED43C718714eb63d5aA57B78B54704E256024E;
+    
+    constructor() ERC20Deflationary(name_, symbol_, decimal_, tokenSupply_) {
+        enableAutoBurn(taxBurn_, taxDecimals_);
+        enableReward(taxReward_, taxDecimals_);
+        enableAutoSwapAndLiquify(taxLiquify_, taxDecimals_, routerAddress, minTokensBeforeSwap_);
     }
 
 }
